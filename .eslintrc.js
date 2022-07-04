237b2a66e9f8bfdeecb949fd4dd02a9db43ac46f
Wonhee Kang <eggfreitag@MacMorning.local>
2022-07-05 01:12:23 +0900

    test: Husky설정 테스트

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "react"],
+  env: {
+    browser: true,
+    jest: true,
+    es6: true,
+  },
 };
