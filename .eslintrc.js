830235a1881c304fc42efa30ce3b3b112c9305f2
Wonhee Kang <eggfreitag@MacMorning.local>
2022-07-05 01:49:36 +0900

    chore: ESLint, Prettier, Husky 설정 완료

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,17 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  plugins: ["prettier", "react"],
+  plugins: ["react", "prettier"],
   env: {
     browser: true,
     jest: true,
     es6: true,
+    node: true,
   },
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {},
 };
