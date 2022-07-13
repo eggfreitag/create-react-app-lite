992a19d6e0d3d3a91725f5b4bca8c360485bef7e
Wonhee Kang <eggfreitag@MacMorning.local>
2022-07-13 22:49:21 +0900

    feat: React-scripts-lite분리 및 경로 수정 완료

--- react-scripts-lite/config/env.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-const env = fs.readFileSync(path.join(__dirname, "../../.env")).toString();
-const envOption = {};
-
-module.exports = envOption;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import styled from "styled-components";
 
 import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
-import { env } from "process";
+
 root.render(
   <>
     <App />
