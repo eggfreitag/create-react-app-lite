5b41117451c1355f3ec569ee8f08adc8604d1cd3
Wonhee Kang <eggfreitag@MacMorning.local>
2022-07-18 23:33:38 +0900

    fix: test가 안되던 버그 해결 및 chalk버그 해결을 위한 dependency변경

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 import App from "../components/App";
 
-describe('should render Edit "src/components/App.js" and save to reload.', () => {
-  render(<App />);
-  const string = screen.getByTestId("Learn React.......");
-  expect(string).toBeInTheDocument();
+describe("rendering", () => {
+  it("should render Edit 'src/components/App.js' and save to reload.", () => {
+    render(<App />);
+    const string = screen.getByText("Learn React.......");
+    expect(string).toBeInTheDocument();
+  });
 });
